229b6f4da09979c2fe23b5182146abc3ee85a4cf
Yeluri-Venkata-Srija <yelurivenkatasrija1617@gmail.com>
2026-04-18 20:21:56 +0530

    created Home Page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Home Page</h1>
+      <p>This is the home section of the portfolio.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,4 @@
+ function Projects() {
+  return <>Projects Page</>
+}
+export default Projects
