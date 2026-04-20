958e9f96498eaec5d81247e8e4f48576d469247f
Yeluri-Venkata-Srija <yelurivenkatasrija1617@gmail.com>
2026-04-20 16:00:33 +0530

    added home page components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import Hero from "./Hero/Hero";
 import About from "./About/About";
 import MyProjects from "./MyProjects/MyProjects";
 import Education from "./Education/Education";
+import Skills from "./Skills/Skills";
+import Contact from "./Contact/Contact";
 
 function Home() {
   return (
@@ -11,7 +13,9 @@ function Home() {
       <Hero />
       <About />
       <Education />
+      <Skills />
       <MyProjects />
+      <Contact />
     </div>
   );
 }
