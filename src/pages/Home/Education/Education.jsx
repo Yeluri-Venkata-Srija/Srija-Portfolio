1050f7f79ddebfe0bb5a7c67746c8f2ee82bc9ee
Yeluri-Venkata-Srija <yelurivenkatasrija1617@gmail.com>
2026-04-20 17:37:16 +0530

    updated education

--- src/pages/Home/Education/Education.jsx
+++ src/pages/Home/Education/Education.jsx
@@ -2,12 +2,38 @@ import "./Education.css";
 
 function Education() {
   return (
-    <section className="education-section">
+    <section className="education-section" id="education">
       <h2>Education</h2>
-      <div className="education-card">
-        <h3>B.Tech - CSE (AI)</h3>
-        <p>KL University (2023–2027)</p>
-        <span>CGPA: 9.03</span>
+
+      <div className="timeline">
+
+        <div className="timeline-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-content">
+            <h3>B.Tech - CSE (AI)</h3>
+            <p>KL University (2023–2027)</p>
+            <span>CGPA: 9.11</span>
+          </div>
+        </div>
+
+        <div className="timeline-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-content">
+            <h3>Intermediate</h3>
+            <p>Rishi Junior College</p>
+            <span>94.8%</span>
+          </div>
+        </div>
+
+        <div className="timeline-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-content">
+            <h3>Schooling</h3>
+            <p>Sarada English Medium High School</p>
+            <span>10 CGPA</span>
+          </div>
+        </div>
+
       </div>
     </section>
   );
